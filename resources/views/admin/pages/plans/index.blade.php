ee748274606460d7658b41221fc2a59697fd3837
João Pedro <jpedrodaibello1999@outlook.com>
2023-03-13 15:07:24 -0300

    feat: add Plan listing

--- resources/views/admin/pages/plans/index.blade.php
+++ resources/views/admin/pages/plans/index.blade.php
@@ -7,5 +7,35 @@
 @stop()
 
 @section('content')
-    <p>Welcome</p>
+    <div class="card">
+        <div class="card-header">
+            #Filtros
+        </div>
+        <div class="card-body">
+            <table class="table table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                        <th style="width: 50px">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($plans as $plan)
+                        <tr>
+                            <td>
+                                {{ $plan->name }}
+                            </td>
+                            <td>
+                                {{ $plan->price }}
+                            </td>
+                            <td style="width: 10px;">
+                                <a href="" class="btn btn-warning">VER</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @stop()
